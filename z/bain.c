dead6e95dd252f61c2ff8ea8576b3e798fa6ee22
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-14 12:51:17 -0700

    add testing file bain.c

--- z/bain.c
+++ z/bain.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include "../main.h"
 #include <stdio.h>
 
 int main(void)
 {
 	_printf("hello its %s starts with %c","abdelhak",'A',"kdkd");
 	_printf("\n");
-	printf("hello its %s starts with %c","abdelhak",'A',"kdkd");
+	_printf("hello its %s starts with %c","abdelhak",'A',"kdkd");
 	printf("\n");
 	return (0);
 }
