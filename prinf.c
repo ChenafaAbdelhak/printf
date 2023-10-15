2e2a73ec669a2f8ccbcbf45c037309c6cbe95873
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-10-15 13:21:41 -0700

    handle NULL string parameter

--- prinf.c
+++ prinf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 
 	va_start(ap, format);
 	if (format == NULL)
-		return (0);
+		return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] != '\0')
@@ -30,7 +30,9 @@ int _printf(const char *format, ...)
 					i++;
 					break;
 				case 's':
-				str = va_arg(ap, char *);
+					str = va_arg(ap, char *);
+					if (str == NULL)
+						str = "(null)";
 					count += _puts(str);
 					i++;
 					break;
--- z/bain.c
+++ z/bain.c
@@ -4,12 +4,14 @@
 int main(void)
 {
 	int n = 0;
+	char *s = NULL;
 
-	n = _printf("hello its %s starts %%with %c %","abdelhak",'A');
+	n = _printf("hello its %s starts %%with %c %s","abdelhak",'A',s);
 	printf("*** %d ***", n);
 	_printf("\n");
+	_printf("Unknown:[%r]\n");
 
-	n = printf("hello its %s starts %%with %c %","abdelhak",'A');
+	n = printf("hello its %s starts %%with %c %s","abdelhak",'A',s);
 	printf("*** %d ***", n);
 	printf("\n");
 
